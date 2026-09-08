8055a425aee38ac198b326ea8d1d802f5ec85111
kpe4 <anafievtimur007@gmail.com>
2026-09-08 14:24:04 +0300

    Create new.py

---
+++ py/new.py
@@ -0,0 +1,25 @@
+# код полчучает данные о компонентах пк, времени. Выводит название системы, версию, архитектуру, дату.
+# Генерирует уникальный айди ситемы из названия системы, названия пк, архитектуры и сетевого идентификатора
+
+import sys
+import platform
+import datetime
+import hashlib
+import uuid
+
+os_name = platform.system()
+os_version = platform.version()
+os_arch = platform.architecture()[0][1]
+
+now = datetime.datetime.now()
+sys_in = sys.path
+print(f"{os_name} {os_version} {os_arch} \n {now.year} {now.month} {now.day} {now.hour}:{now.minute}")
+
+info = (
+    platform.node()
+    + platform.system()
+    + platform.machine()
+    + str(uuid.getnode())
+)
+system_id = hashlib.sha256(info.encode()).hexdigest()
+print("System ID:", system_id)
